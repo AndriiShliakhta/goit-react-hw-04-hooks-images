6fd098c83019045fae5b8c3f7c4736b42906e76e
Andrew <andrew_shliahta@ukr.net>
2021-11-08 08:36:27 +0200

    3.2

--- src/ImageGallery/ImageGallery.js
+++ src/ImageGallery/ImageGallery.js
@@ -14,55 +14,32 @@ export default function ImageGallery({ imageName, openModal }) {
     setPage(prev => prev + 1);
   };
 
-  // useEffect(() => {
-  //   if (!imageName) {
-  //     return;
-  //   }
-  //   setImages(null);
-  //   setPage(1);
-
-  //   console.log(page);
-  //   getData();
-  // }, [imageName, page]);
-
   useEffect(() => {
-    if (!imageName) {
-      return;
-    }
-    setImages(null);
-    setPage(1);
-
-    getData();
+    getData(1);
+    return;
   }, [imageName]);
 
   useEffect(() => {
+    page !== 1 && getData(page);
+  }, [page]);
+
+  const getData = page => {
     if (!imageName) {
       return;
     }
-    getData();
-  }, [page]);
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (
-  //     prevState.page !== page ||
-  //     prevProps.imageName !== imageName
-  //   ) {
-  //     // this.getData();
-
-  //     if (prevProps.imageName !== this.props.imageName) {
-  //       this.setState({ images: null, page: 1 });
-  //     }
-  //   }
-  // }
-
-  const getData = () => {
     setLoading(true);
     axios
       .get(
         `https://pixabay.com/api/?q=${imageName}&page=${page}&key=23115860-3b173cd8cbd28dc69cb35b572&image_type=photo&orientation=horizontal&per_page=12`,
       )
       .then(resp => {
-        setImages(page === 1 ? resp.data.hits : [...images, ...resp.data.hits]);
+        if (page === 1) {
+          setPage(1);
+          setImages(resp.data.hits);
+        } else {
+          setImages(prev => [...prev, ...resp.data.hits]);
+        }
+
         window.scrollTo({
           top: document.documentElement.scrollHeight,
           behavior: 'smooth',
@@ -74,6 +51,7 @@ export default function ImageGallery({ imageName, openModal }) {
 
   return (
     <>
+      {' '}
       <ul className="ImageGallery" onClick={openModal}>
         {error && <h1>{error.message}</h1>}
         {loading && <SpinnerDotted />}
@@ -92,52 +70,3 @@ export default function ImageGallery({ imageName, openModal }) {
     </>
   );
 }
-
-// class ImageGallery extends Component {
-//   state = {
-//     images: null,
-//     loading: false,
-//     error: null,
-//     page: 1,
-//   };
-
-//   loadMore = () => {
-//     this.setState(prev => ({ page: prev.page + 1 }));
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (
-//       prevState.page !== this.state.page ||
-//       prevProps.imageName !== this.props.imageName
-//     ) {
-//       this.getData();
-
-//       if (prevProps.imageName !== this.props.imageName) {
-//         this.setState({ images: null, page: 1 });
-//       }
-//     }
-//   }
-
-//   getData = () => {
-//     this.setState({ loading: true });
-
-//     axios
-//       .get(
-//         `https://pixabay.com/api/?q=${this.props.imageName}&page=${this.state.page}&key=23115860-3b173cd8cbd28dc69cb35b572&image_type=photo&orientation=horizontal&per_page=12`,
-//       )
-//       .then(resp => {
-//         this.setState(prev => ({
-//           images:
-//             prev.page === 1
-//               ? resp.data.hits
-//               : [...prev.images, ...resp.data.hits],
-//         }));
-//         window.scrollTo({
-//           top: document.documentElement.scrollHeight,
-//           behavior: 'smooth',
-//         });
-//       })
-//       .catch(error => this.setState({ error }))
-//       .finally(this.setState({ loading: false }));
-//   };
-// }
